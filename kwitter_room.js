40255fc17b73df95b955f3f74782a8dbfd41737c
Rubikspro123 <87181104+Rubikspro123@users.noreply.github.com>
2022-01-29 13:22:31 +0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,25 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+      apiKey: "AIzaSyDbfe7sjbfY3ack_iLAEp84QWoiQ_CYiQY",
+      authDomain: "kwitterakshit.firebaseapp.com",
+      projectId: "kwitterakshit",
+      storageBucket: "kwitterakshit.appspot.com",
+      messagingSenderId: "41193646220",
+      appId: "1:41193646220:web:367aa3a021206acb61f3a9"
+    };
+    
+    // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//ADD YOUR FIREBASE LINKS HERE
+function logout(){
+      window.location = "index.html";
+}
+function getData() {
+      firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";
+      snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+      
+      //End code
+      });});}
+getData();
